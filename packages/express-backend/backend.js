80d5124f3e664bb1e2c0ee2b5daa8ae9a7b004d4
Bernice Lau <bernicelau@Bernices-MacBook-Pro-2.local>
2024-02-01 12:16:17 -0800

    set up database access on backend with starter code

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -81,6 +81,7 @@ const deleteUserById = (id) => {
 
 
 // --- Routes --- //
+
 app.use(cors());
 app.use(express.json());
 
@@ -88,37 +89,36 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });  
   
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    const job = req.query.job;
-
-    if (name !== undefined && job !== undefined) {
-        let result = findUsersByNameAndJob(name, job);
-        result = { users_list: result };
-        res.send(result);
-    } else if (name !== undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
+app.get("/users", async (req, res) => {
+    const name = req.query["name"];
+    const job = req.query["job"];
+    try {
+      const result = await userServices.getUsers(name, job);
+      res.send({ users_list: result });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("An error ocurred in the server.");
     }
 });
 
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
     const id = req.params["id"];
-    let result = findUserById(id);
-    if (result === undefined) {
-        res.status(404).send("Resource not found.");
-    } else {
-        res.send(result);
+    const result = await userServices.findUserById(id);
+    if (result === undefined || result === null)
+      res.status(404).send("Resource not found.");
+    else {
+      res.send({ users_list: result });
     }
 });
 
-app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    const addedUser = addUser(userToAdd);
-    res.status(201).json(addedUser);
+app.post("/users", async (req, res) => {
+    const user = req.body;
+    const savedUser = await userServices.addUser(user);
+    if (savedUser) {
+        res.status(201).send(savedUser);
+    } else {
+        res.status(500).end();
+    }
 });
 
 app.delete("/users/:id", (req, res) => {
